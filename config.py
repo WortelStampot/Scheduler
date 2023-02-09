ff68168cb9a57d89d7f94db7e3141920e1e7a89d
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-02-09 12:06:50 -0500

    config has schema

---
+++ config.py
@@ -0,0 +1,47 @@
+
+class DefaultConfig:
+    SCHEMA = {
+        "roles": [
+            {
+                "name": str(),
+                "callTime": str(),
+                "qualifiedStaff": [
+                    str()
+                ],
+                "day": str()
+            }
+        ],
+        "staff": [
+            {
+                "name": str(),
+                "maxShifts": int(),
+                "availability": {
+                    "MONDAY": [
+                        str()
+                    ],
+                    "TUESDAY": [
+                        str()
+                    ],
+                    "WEDNESDAY": [
+                        str()
+                    ],
+                    "THURSDAY": [
+                        str()
+                    ],
+                    "FRIDAY": [
+                        str()
+                    ],
+                    "SATURDAY": [
+                        str()
+                    ],
+                    "SUNDAY": [
+                        str()
+                    ]
+                },
+                "rolePreference": [
+                    str()
+                ],
+                "doubles": bool()
+            }
+        ]
+    }
