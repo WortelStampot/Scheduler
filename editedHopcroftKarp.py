f74232e8af26bb5d0778ee55bd6f46b3c2a7ab96
Sil Demmer <Sil@Hey.com>
2023-05-02 16:45:58 -0400

    making sense of the matching algorithm

--- editedHopcroftKarp.py
+++ editedHopcroftKarp.py
@@ -5,7 +5,7 @@ from networkx.algorithms.bipartite import sets as bipartite_sets
 
 INFINITY = float("inf")
 
-def availabilityMatching(G, top_nodes=None):
+def availabilityMatching(Graph, top_nodes=None):
 
     """Returns the maximum cardinality matching of the bipartite graph `G`.
 
@@ -69,54 +69,76 @@ def availabilityMatching(G, top_nodes=None):
     """
     
     def breadth_first_search():
-        for v in staffNodes:
-            #if this staff node has no identified matches yet, we add it to the 'distances' dictionary with a value of 0
+        for staff in staffNodes: #this loop populates the queue and distances dictionary for each staff in the staff node set.
+            #if this staff node has no identified match, we add it to the 'distances' dictionary with a value of 0
+            #At the start of this function, this True for all staff nodes in the staff nodes set.
             #this staff node (object?) then gets appended to the 'queue'.
-            if staffMatches[v] is None:
-                distances[v] = 0
-                queue.append(v)
-            #However, when the value of this staff in the staffMatches dictionary is not None (presume this means there is a match for this staff?),
+            if staffMatches[staff] is None:
+                distances[staff] = 0
+                queue.append(staff)
+            #However, when the value of this staff in the staffMatches dictionary is not None
             #we set this staff's value in the 'distances' dictionary to INFINITY.
-                # My question so far, what is this 'distances' dictionary?
+                # My question here, what is this 'distances' dictionary?
                 # What is it tracking the state of? Distance from this staff node to what?
             else:
-                distances[v] = INFINITY
-        distances[None] = INFINITY
-        while queue:
-            v = queue.popleft()
-            if distances[v] < distances[None]:
-                for u in G[v]:
-                    if distances[roleMatches[u]] is INFINITY:
-                        distances[roleMatches[u]] = distances[v] + 1
-                        queue.append(roleMatches[u])
-        return distances[None] is not INFINITY
+                distances[staff] = INFINITY
+        distances[None] = INFINITY # here we add a 'None' node to the distances dictionary with a value of INFINITY.
+        #What is the function of this None node? What is it setting us up for?
+        while queue: #this loop checks if there is a staff node in the set, for whom to identify a connecting edge (this will be done with the depth-first-search). 
+            staff = queue.popleft()
+            if distances[staff] < distances[None]: # if the distance value of staff is less than INFINITY
+                #Would 'if distances[staff] is not INFINITY' be another way to write this?
+                debugGraph = Graph[staff]
+                for role in Graph[staff]: # In this loop we go through each role this staff is connected (adjacent?) to.
+                    #the staff node being the dictionary key to get into a deeper dictionary of the graph data strucutre. A dictionary of connected role nodes for each staff.
+                    if distances[roleMatches[role]] is INFINITY: # when the value of roleMatches[role] is None, this expression evaluates to: distances[None]
+                        #this means there is yet to be a match for this role.
+                        #And this is what the None node above has been established for?
+                        #since now this expression returns True as distances[None] is INFINITY?
+                        distances[roleMatches[role]] = distances[staff] + 1 # now we incremint the distance value of the None node by the distance value of the staff Node (0) + 1.
+                        #what happens is this sets the None node value from inf to 1.
+                        #this is important since flipping the None node from inf to 1 informs us 'hey, there is a staff and role node in these sets for whom to perform a depth first search'
 
-    def depth_first_search(v):
-        if v is not None:
-            for u in G[v]:
-                if distances[roleMatches[u]] == distances[v] + 1:
-                    if depth_first_search(roleMatches[u]):
-                        roleMatches[u] = v
-                        staffMatches[v] = u
-                        return True
-            distances[v] = INFINITY
-            return None
-        return True
+        return distances[None] is not INFINITY #returning True now that distances[None] is 1 (not INFINITY).
+    #this saying, True- there are at least one staff and role nodes in these two sets 'unchecked' for a match.
+    #which translates to saying True- it makes sense to go on and perform the depth first search.
+
+    def depth_first_search(staff): #At this point we have identified this is a staff whom to do a search for.
+        if staff is not None:
+            for role in Graph[staff]: #for each role this staff is connected to.
+                if distances[roleMatches[role]] == distances[staff] + 1: #when the value of None node is 1 and value of staff distance is 0, this returns True. 
+                    if depth_first_search(roleMatches[role]): #if this role also doesn't have a match yet,
+                        roleMatches[role] = staff #this staff is set as a match with this role
+                        staffMatches[staff] = role #this role is set to match with this staff
+                        #In other words, this confirms an edge exists between the the role and staff node which has been established earlier.
+                        #So a 'match' is actually the first occuring edge between an 'open' (yet unmatched) role and staff node in the graph?
+                        return True #and returning True, takes us out of the function, allows us to increment the num_matched_pairs counter, and go through these steps again with the following staff.
+                    
+            distances[staff] = INFINITY #when going through all the connected roles of a staff, and there is no corosponding 'open' connected role, we hit this expression
+            return None #and return None.
+        return True #this line is hit when the staff variable is 'None'?
+    #When would there be a 'None' node in the staffNodes list?
 
     # Initialize the "global" variables that maintain state during the search.
-    staffNodes, roleNodes = bipartite_sets(G, top_nodes)
-    staffMatches = {v: None for v in staffNodes}
-    roleMatches = {v: None for v in roleNodes}
-    distances = {}
+    staffNodes, roleNodes = bipartite_sets(Graph, top_nodes)
+    staffMatches = {staff: None for staff in staffNodes}
+    roleMatches = {role: None for role in roleNodes}
+    distances = {} # What this dictionary represents is still fuzzy to me.
     queue = collections.deque()
 
     # Implementation note: this counter is incremented as pairs are matched but
     # it is currently not used elsewhere in the computation.
     num_matched_pairs = 0
-    while breadth_first_search():
-        for v in staffNodes:
-            if staffMatches[v] is None:
-                if depth_first_search(v):
+    while breadth_first_search(): #while there are at least one staff and role node in the two sets 'unchecked' for a match.
+        # rephrasing: while distances[None] is not INFINITY
+        #This is the 'breadth' part of the search. Checking a surface 'True/False' across the two sets of node, for whether to continue searching.
+        #This is also why the staff nodes need to be unique objects? Since each staff object (node) gets its own distance value?
+
+        for staff in staffNodes: #go through each staff node in the staff node set.
+            if staffMatches[staff] is None: # This statement culls from a 'yes there is a staff for whom to perform the deeper search for in the set,
+                #down to 'this is the staff for whom to perform a depth first search for.'
+                if depth_first_search(staff): #This statement is True when a depth first search results in identifing a match with this staff to a role node.
+                    #this depth first search function also 'sets' the match value (when a match is found) in the staffMatches/roleMatches dictionaries.
                     num_matched_pairs += 1
 
     return roleMatches
