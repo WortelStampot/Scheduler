541741bb5b271035440397095176ce4ffd879ae6
Sil Demmer <Sil@Hey.com>
2023-06-14 15:51:56 -0400

    while arranging, removing log statements and related function calls

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ logger = logging.getLogger(__name__)
 
 def createSchedule(roleCollection, staffCollection):
     schedule = Schedule(roles=roleCollection, staff=staffCollection)
-    
-    unavailables = schedule.identifyUnavailables()
-    logger.info(f'Unavailables after Matching: {len(unavailables), unavailables}')
-
     schedule.repairDoubles()
-    doubles = schedule.identifyDoubles()
-    logger.info(f'Doubles after repairing: {len(doubles), doubles}')
-    logger.info(f'unmatched role: {schedule.unMatchedRoles}')
     
     return schedule
