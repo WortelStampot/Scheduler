ac5d38c708171c1be9314d7c9dbeddf980643aeb
Sil Demmer <Sil@Hey.com>
2023-06-16 17:57:04 -0400

    added duplicateStaff() This probably belongs in graphFunctions section

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ from matching import availabilityMatching
 
 logger = logging.getLogger(__name__)
 
-def createSchedule(roleCollection, staffCollection):
+def createSchedule(roleCollection, staffCollection):    
     schedule = Schedule(roles=roleCollection, staff=staffCollection)
     schedule.repairDoubles()
     
     return schedule
+
+
+def duplicateStaff(staffCollection):
+        '''
+        duplicate Staff by the number of shifts they are available to work
+        '''
+        staffByShifts = []
+        for staff in staffCollection:
+            shiftsRemaining = min(staff.maxShifts(), staff.daysAvailable())
+            for shiftCount in range(shiftsRemaining):
+                staffByShifts.append(copy.deepcopy(staff))
+            logger.debug(f'{staff} duplicated by: {shiftCount}')
+        return staffByShifts
