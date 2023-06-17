76abb376f09595670135a167a18e866ae0234efb
Sil Demmer <Sil@Hey.com>
2023-06-17 13:04:15 -0400

    shaping the starting schedule

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import logging
 from classes import Weekdays, Schedule
 import networkx as nx
+from networkx.algorithms import bipartite
 import copy
 from matching import availabilityMatching
 
@@ -9,41 +10,50 @@ logger = logging.getLogger(__name__)
 
 def createSchedule(roleCollection, staffCollection):    
     schedule = Schedule(roles=roleCollection, staff=staffCollection)
+
     schedule.repairDoubles()
     
     return schedule
 
 def createStartingSchedule(roleCollection, staffCollection):
-    #Here we create starting schedule, this is a schedule made by matching roles with staff based on availability
-    #within this scope we have decided our starting point is specifically based on availability.
-    
-    staffCollection = duplicateStaff(staffCollection) # this could be done on the appscript end,
+    """
+    Create a starting schedule by matching Roles with Staff based on availability
+
+    return:
+    Schedule object
+    """
+    ScheduleObject = Schedule() #empty while arranging
+    #Create a scheulde object here? Still hazy on what a Schedule object is to be in our context.
+
+    staffCollection = duplicateStaff(staffCollection) # This could be done on the appscript end?
     #when things get settled, maybe move this funcationality over there.
 
-    #to start, we create a graph
     Graph = nx.Graph()
-    Graph.add_nodes_from(roleCollection, bipartite=0) #and Role and Staff nodes
+    Graph.add_nodes_from(roleCollection, bipartite=0)
     Graph.add_nodes_from(staffCollection, bipartite=1)
+    left,right = nx.bipartite.sets(Graph) # debugging
 
-    #now we have to edges to the graph to get any use out of it.
-    #these edges represent which Role a Staff is available for based on Role.callTime and Staff.Availability
-    roleStaffConnections = []
+    availabilityEdges = []  
     for staff in staffCollection:
         for role in roleCollection:
             if staff.isAvailable(role):
-                roleStaffConnections.append((role, staff))
+                availabilityEdges.append((role, staff))
 
-    #with the edges identified we add them to the graph.      
-    Graph.add_edges_from(roleStaffConnections)
+    Graph.add_edges_from(availabilityEdges)
 
-    #the graph is complete.
-    #and ready to be used by our matching algorithm.
-    startingSchedule = availabilityMatching(Graph)
+    roleMatches = availabilityMatching(Graph) #TODO: set top nodes
+    nxMatches = bipartite.maximum_matching(Graph) # this would be using networkx as is.
+    #The reason I'm not going this route is that it returns a combined dictionary of 'left' and 'right' matches with 'None' stripped out.
+    #when all we want, I think, is one set with unmatched Roles still in place.
+    # By pasting the source code and cutting it a bit early to return 'roleMatches', we get what we're looking for- I think.
 
-    Schedule = classes.Schedule() # this is the point where a 'Schedule' object is created.
-    Schedule.schedule = startingSchedule
+    startingSchedule = {Role: Staff for Role, Staff in roleMatches.items() if Staff is not None} #strip unmatched roles
+    unassignedRoles = {Role for Role, Staff in roleMatches.items() if Staff is None}
 
-    return Schedule # from this point on, we can work with the Schedule object directly.
+    ScheduleObject.schedule = startingSchedule # this is the first time we have a 'schedule'
+    ScheduleObject.unassigned = unassignedRoles # Having access to unassigned Roles seems important
+
+    return ScheduleObject # from this point on, we can work with the Schedule object directly.
 
     #EDGE CASE:
     #When there is a Role which no Staff is available for we could notice at this point.
@@ -56,10 +66,18 @@ def createStartingSchedule(roleCollection, staffCollection):
 
     #OBSERVATIONS:
     #At this point, there is a starting schedule.
-    #What actually is a Schedule- as an object
-    #at this point it's a dictionary which holds {Role: Staff} pairs.
-    #This object gets created at which point? inside the matching algorithm?
-    #The matching algorithm traditionally returns a dictionary like this.
+    #What actually is a Schedule- as an object.
+    # Schedule.schedule is a dictionary which holds {Role: Staff} pairs.
+    # and from this point on, can be iterated on.
+    # Schedule.roles is a collection of Role objects for the week.
+    # Schedule.staff is a collection of Staff object for the week. 
+        #This diverges a bit since in createStartSchedule we duplicate each Staff by their 'shiftsRemaining'
+        #this actually creates new unique Staff objects which are now dispersed throughout Schedule.schedule
+        #I'd like to avoid this. Personally I still think of the StaffCollection as a list of unique Staff objects, unduplicated.
+        # while practically I don't yet know how to set that up with the matching algorithm, and I don't know how important it is.
+        # So- for now Schedule.staff = the duplicated list of the Staff collection?
+ 
+    #The networkx matching algorithm traditionally returns a dictionary like this.
     #However the networkx implementation leaves out unmatched nodes.
     #in our case, this means any Roles which do not appear in the matching result will be 'unmatched roles'
     #this is something worth identifying. However, there is no reason to chop up the networkx algorithm.
