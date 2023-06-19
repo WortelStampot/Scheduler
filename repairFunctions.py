236c8e1a4f85319bb35b49a9a7ce44f58ec75d04
Sil Demmer <Sil@Hey.com>
2023-06-19 15:43:13 -0400

    repairDoubles using a Schedule object

--- repairFunctions.py
+++ repairFunctions.py
@@ -1,44 +1,24 @@
-def identifyUnavailables(self):
-    """
-    Return list of all roles where the staff is unavailable to work the role
-    """
-    return [role for role, staff in self.schedule.items() if not staff.isAvailable(role)]
+import logging
+import random
+logger =  logging.getLogger(__name__)
 
-def identifyDoubles(self):
-    """
-    return list of roles that need to be reassigned to avoid doubles
-    """
-    
-    #if staff has already worked that day, then it's a double
-    doubles = []
-    staffDays = set() #set of staff day pairs
-    for role, staff in self.schedule.items():
-        day = role.day
-        staffDay = (staff.name, day)
-
-        if staffDay in staffDays:
-            doubles.append(role)
-        else:
-            staffDays.add(staffDay)
-    return doubles
-
-def repairDoubles(self):
-    doubles = self.identifyDoubles()
-    self.unrepairedDoubles = list() #attaching to schedule for now
+def repairDoubles(Schedule):
+    doubles = identifyDoubles()
+    Schedule.unrepairedDoubles = list() #TODO: sort this out- attaching to schedule for now
     logger.info(f"repairDoubles starting count: {len(doubles)}\n{doubles}")
 
     MAX_ATTEMPTS = 100 #it now seems unlikely for 100 attempts to be reached with ~89 Roles in a weekly roleCollection
     attempts = 0
     while doubles != [] and attempts < MAX_ATTEMPTS:
         doubleRole = random.choice(doubles) #select a random double from the list
-        self.repairDouble(doubleRole) #start repair process for selected double
+        Schedule.repairDouble(doubleRole) #start repair process for selected double
 
-        doubles = [role for role in self.identifyDoubles() if role not in self.unrepairedDoubles] #recompute the list of doubles, leaving out perviously unrepaired roles.
+        doubles = [role for role in Schedule.identifyDoubles() if role not in Schedule.unrepairedDoubles] #recompute the list of doubles, leaving out perviously unrepaired roles.
 
         attempts += 1
         logger.debug(f"doubles attempts: {attempts}")
     
-    endingDoubles = self.identifyDoubles() #logging the number of doubles after the repairDoubles process.
+    endingDoubles = Schedule.identifyDoubles() #logging the number of doubles after the repairDoubles process.
     if endingDoubles != []:
         logger.warning(f"repairDoubles complete. remaining doubles: {len(endingDoubles)}\n{endingDoubles}")
     else:
@@ -47,13 +27,13 @@ def repairDoubles(self):
     print(f'repairDoubles complete. remaining doubles: {len(endingDoubles)}')
 
 
-def repairDouble(self, doubleRole):
-    staff = self.schedule[doubleRole]
+def repairDouble(Schedule, doubleRole):
+    staff = Schedule.schedule[doubleRole]
     logger.info(f"Double role to repair: {doubleRole}, {staff}")
 
     #log staff schedule
     logger.debug(f'{staff} Schedule:')
-    shifts = staff.scheduleView(self.schedule)
+    shifts = staff.scheduleView(Schedule.schedule)
     for shift in shifts:
         logger.debug(f'{shift}')
     #log staff availability
@@ -61,14 +41,14 @@ def repairDouble(self, doubleRole):
         logger.debug(f'{day.name, avail}')
 
     try: #creating the doubles graph when it doesn't yet exist
-        self.graph
+        Schedule.graph
     except AttributeError:
-        self.graph = {role1: {role2: self.StaffIsAvailableFor_Day(staff1,role2) for role2 in self.schedule} for role1, staff1 in self.schedule.items()}
+        Schedule.graph = {role1: {role2: Schedule.StaffIsAvailableFor_Day(staff1,role2) for role2 in Schedule.schedule} for role1, staff1 in Schedule.schedule.items()}
 
     MAX_LENGTH = 6 #reasonablly setting a limit of the cycles we're willing to search for within the graph.
     for length in range(2,MAX_LENGTH):
         logger.info(f"finding all cycles of length: {length}")
-        allCycles = self.allCyclesOfLength(doubleRole, length)
+        allCycles = Schedule.allCyclesOfLength(doubleRole, length)
 
         if allCycles == []: # when no cycles found, increment length and do a deeper search.
             logger.warning(f"no cycles for length:{length}")
@@ -78,9 +58,28 @@ def repairDouble(self, doubleRole):
         cycle = random.choice(allCycles) # select a random cycle from the list
         logger.info(f'selected cycle: {cycle}') #show selected cycle
 
-        self.cycleSwap(cycle) #swap the staff within the cycle
+        Schedule.cycleSwap(cycle) #swap the staff within the cycle
         return
     
     #when no cycles are found within the MAX_LENGTH limit, we come here, leaving the double unrepaired
     logger.warning(f"{doubleRole},{staff} left unrepaired.")
-    self.unrepairedDoubles.append(doubleRole)
+    Schedule.unrepairedDoubles.append(doubleRole)
+
+
+def identifyDoubles(Schedule):
+    """
+    return list of roles that need to be reassigned to avoid doubles
+    """
+    
+    #if staff has already worked that day, then it's a double
+    doubles = []
+    staffDays = set() #set of staff day pairs
+    for role, staff in Schedule.schedule.items():
+        day = role.day
+        staffDay = (staff.name, day)
+
+        if staffDay in staffDays:
+            doubles.append(role)
+        else:
+            staffDays.add(staffDay)
+    return doubles
