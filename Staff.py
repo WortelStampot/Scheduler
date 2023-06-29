cecd4db369e00843b9188488be108a24c1892090
Sil Demmer <Sil@Hey.com>
2023-06-29 15:00:55 -0400

    removed type hinting It's a nice idea that I can't get to work without adding import cluttter. Which goes against keeping a readable main

--- Staff.py
+++ Staff.py
@@ -1,6 +1,4 @@
 from Weekdays import Weekdays
-from Role import Role
-from Schedule import Schedule
 import logging
 logger = logging.getLogger(__name__)
 
@@ -19,14 +17,14 @@ class Staff:
 		return f"{self.name}"
 
 
-	def isAvailable(self, role: 'Role'):
+	def isAvailable(self, role):
 		""""check role callTime is in staff availablity"""
 		dayAvailability = self.availability[role.day]
 		if role.callTime not in dayAvailability:
 			return False
 		return True
 
-	def shifts(self, schedule: Schedule):
+	def shifts(self, schedule):
 		"""
 		returns a list of roles this staff is currently scheduled for
 		NOTE: a scheduled role is found from the schedule using staff.name
@@ -38,7 +36,7 @@ class Staff:
 		shifts.sort(key=lambda role: role.day.value)
 		return shifts
 
-	def shiftsRemaining(self, schedule: Schedule):
+	def shiftsRemaining(self, schedule):
 		"""
 		returns number of shifts remaining based on this staff's maxShifts value
 		NOTE: a matching shift is found from the schedule using staff.name 
@@ -65,7 +63,7 @@ class Staff:
 
 		return daysAvailable
 
-	def isOpenFor(self, role: Role, schedule: Schedule):
+	def isOpenFor(self, role, schedule):
 		"""
 		Used to create a graph representing which Roles a Staff is 'open to swap with'.
 		Returns True when Staff is not working on Role.day and Staff is available for Role.callTime
