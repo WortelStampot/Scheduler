ff6bd831a0a38551ab50649a63dd2785988a8949
Sil Demmer <Sil@Hey.com>
2023-06-30 17:32:41 -0400

    WIP - getting schedule as a dictionary to use in further tests

--- tests/requestTests.py
+++ tests/requestTests.py
@@ -1,4 +1,5 @@
 import requests # using requests post to localhost.
+import json
 # this means flask needs to be running: flask --debug run
 # TODO https://flask.palletsprojects.com/en/2.3.x/testing/
 
@@ -11,7 +12,15 @@ with open(ROLE_STAFF_FILE_NAME) as payload:
     """test the post command received a response."""
     assert response.status_code == 200
 
+    """test app.createSchedule returns json string"""
+    schedule = response.json()
+    json.load(schedule)
+    #TODO: get schedule as dictionary.
 
+    """test main.createSchedule returns a Schedule object"""
+        # I don't see how this setup allows me to test for this.
+        # the response from requests.post contains the json string returns by app.py's createSchedule.
+        # how do I write test for main.py's createSchedule?
 
 
 
