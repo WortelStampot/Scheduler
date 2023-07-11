1074edd0dbd9c37244fb9d9a1dd30e71f125bb29
Sil Demmer <Sil@Hey.com>
2023-07-11 16:49:38 -0400

    outline for test cases

---
+++ functionTests.py
@@ -0,0 +1,26 @@
+from Staff import Staff
+from Role import Role
+from Schedule import Schedule
+
+
+#Test cases for staff.isOpenFor()
+
+"""
+Staff is not working that day and they are available
+return True
+"""
+
+"""
+Staff is working that day and they are available
+return False
+"""
+
+"""
+Staff is not working that day and they are not available
+return False
+"""
+
+"""
+Staff is working the role
+return False
+"""
