67bb0b68e4d5e654c79ca402433c7ba9307b6962
Sil Demmer <Sil@Hey.com>
2023-07-12 18:11:35 -0400

    added matchingQualties for maximumMatching

--- Schedule.py
+++ Schedule.py
@@ -2,6 +2,7 @@ import logging
 from Weekdays import Weekdays
 from graphFunctions import maximumMatching
 import copy
+from Staff import Staff
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +23,12 @@ class Schedule:
 		staffCollection = duplicateStaff(self.staff) # REASON: the matching algorithm requires each node in the 'staff set' to be unqiue.
 		#duplicating here leaves schedule.staff as a list of individual staff objects outside of this function
 
-		matching = maximumMatching(self.roles, staffCollection) # returns complete matching 'left' and 'right'
+		matchingQualities = [ #the idea is to implement multiple 'matchingQualities' in a readable fashion.
+			Staff.isAvailable,
+			Staff.isQualified
+			]
+		
+		matching = maximumMatching(self.roles, staffCollection, matchingQualities) # returns complete matching 'left' and 'right'
 
 		return {Role: Staff for Role, Staff in matching.items() if Role in self.roles} # get half of the matching dictionary
 	
