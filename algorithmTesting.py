77b2a6b84883777ca99d17cb978b567b6cc0cc2a
Sil Demmer <Sil@Hey.com>
2023-07-31 13:33:24 -0400

    filling out toCSV()

--- algorithmTesting.py
+++ algorithmTesting.py
@@ -1,4 +1,6 @@
 from graphFunctions import weightedMatching, bipartiteMatching # this can change
+from Weekdays import Weekdays
+import csv
 roles = 'roleCollection'
 staff = 'staffCollection'
 
@@ -9,27 +11,35 @@ class algorithmTest(roles, staff):
 
         return algorithm(roles, staff)
 
-    def toCSV(matching):
-        """
-        create a csv displaying the matching results of role and staff data
-        """
+def toCSV(matching):
+    """
+    create a csv displaying the matching results of role and staff data
+    """
 
-        '''Display the roles in groupings per day, ordered by role callTimes-
-        multiple of the same role names grouped together'''
+    '''Display the roles in groupings per day, ordered by role callTimes-
+    multiple of the same role names grouped together'''
+    with open('matching.cvs', 'w', newline='') as csvFile:
+        shiftWriter = csv.writer(csvFile)
 
-        '''Display the roles in groupings per staff, ordered by weekday'''
-
-        '''Display the number times a staff is scheduled for the same role in a week'''
-
-
-
-
-weightedMatching(roles, staff)
-bipartiteMatching(roles, staff)
+        for day in Weekdays:
+            dayShifts = [shift for shift in matching if shift.day == day]
+            shiftWriter.writerow(dayShifts)
 
 
-algorithmTest(roles, staff, weightedMatching).toCSV()
-algorithmTest(roles, staff, bipartiteMatching).toCSV() # no not quite.
+    '''Display the roles in groupings per staff, ordered by weekday'''
+
+    '''Display the number times a staff is scheduled for the same role in a week'''
+
+
+matching = bipartiteMatching(roles, staff)
+toCSV(matching)
+
+
+# bipartiteMatching(roles, staff)
+
+
+# algorithmTest(roles, staff, weightedMatching).toCSV()
+# algorithmTest(roles, staff, bipartiteMatching).toCSV() # no not quite.
 
 
 
