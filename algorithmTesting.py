b9b651d72009c2d741bb8a9297a77e6489688bdb
Sil Demmer <Sil@Hey.com>
2023-08-07 16:11:31 -0400

    in progress: organizing input/output functions

--- algorithmTesting.py
+++ algorithmTesting.py
@@ -1,38 +1,6 @@
 from MatchingAlgorithms import MatchingAlgorithms
-from parsingFunctions import parseRole, parseStaff
-from Schedule import Schedule
 from pathlib import Path
 from repairDoubles import repairDoubles
-import json
-import csv
-
-def scheduleFrom(jsonFile, matchingAlgorithm):
-    """
-    create a schedule object from json input with specified matching algorithm
-    """
-    with open(jsonFile) as file:
-        scheduleData = file.read()
-        schedule = json.loads(scheduleData)
-        roles = [parseRole(role) for role in schedule["roles"]]
-        staff = [parseStaff(staff) for staff in schedule["staff"]]
-
-        return Schedule(roles=roles, staff=staff, matchingAlgorithm=matchingAlgorithm )
-
-def writeCSV(schedule, outputName):
-    """
-    write a schedule's schedule to a csv file with specified output name
-      #TODO: schedule input information inside Schedule?
-      e.g. schedule.Date from incomming json    
-    """
-    OUTPUT_DIR = 'Output/'
-    file = ''.join([OUTPUT_DIR, outputName,'.csv'])
-
-    with open(file, 'w', newline='') as csvFile:
-        csvWriter = csv.writer(csvFile)
-        csvData = schedule.toCSV()
-
-        for row in csvData:
-            csvWriter.writerow(row)
 
 ''' ---- set input here ---- '''
 jsonFile = 'Input/roleStaff_5_29_pref.json'
--- scheduleTest_Local.py
+++ scheduleTest_Local.py
@@ -1,80 +1,118 @@
-import parsingFunctions
-import main
+from parsingFunctions import parseRole, parseStaff
+from Schedule import Schedule
 import os
 import json
+import csv
 
-def createSchedule(filePath): #eek another 'createSchedule' name
-    """
-    create a schedule object from input data
-    """
-    with open(filePath) as file:
-        scheduleData = file.read()
-        schedule = json.loads(scheduleData)
-        roleCollection = [parsingFunctions.parseRole(role) for role in schedule["roles"]]
-        staffCollection = [parsingFunctions.parseStaff(staff) for staff in schedule["staff"]]
-        schedule = main.createSchedule(roleCollection, staffCollection)
+"""
+functions for creating and interacting with a schedule object locally
+"""
 
-        return schedule
-
-def scheduleTests(schedule):
+class Tests:
     """
-    tests for a schedule object
+    verify and validate process results on a schedule object
     """
 
-    """Test for less than 2 doubles in schedule"""
-    try:
-        assert len(schedule.identifyDoubles()) < 2
-    except AssertionError:
-        print(f'doubles check failed: {len(schedule.identifyDoubles())}')
-        return False
-    print('doubles check: pass')
+    def scheduleTests(schedule):
+        """
+        tests for a schedule object
+        """
 
-    """Test for each staff paired with a role they are available for"""
-    for role, staff in schedule.schedule.items():
+        """Test for less than 2 doubles in schedule"""
         try:
-            assert staff.isAvailable(role)
+            assert len(schedule.identifyDoubles()) < 2
         except AssertionError:
-            print(f'availability check failed for:\n {role, staff}')
+            print(f'doubles check failed: {len(schedule.identifyDoubles())}')
             return False
-    print('availabiliy check: pass')
+        print('doubles check: pass')
 
-    """Test each staff is qualified for their matched role"""
-    for role, staff in schedule.schedule.items():
-        try:
-            assert staff.isQualified(role)
-        except AssertionError:
-            print(f'qualified check failed for:\n {role, staff}')
-            return False
-    print('qualified check: pass')
+        """Test for each staff paired with a role they are available for"""
+        for role, staff in schedule.schedule.items():
+            try:
+                assert staff.isAvailable(role)
+            except AssertionError:
+                print(f'availability check failed for:\n {role, staff}')
+                return False
+        print('availabiliy check: pass')
 
-    #print(f'test passed for {file.name}') TODO: print out file name being used
-    return True
+        """Test each staff is qualified for their matched role"""
+        for role, staff in schedule.schedule.items():
+            try:
+                assert staff.isQualified(role)
+            except AssertionError:
+                print(f'qualified check failed for:\n {role, staff}')
+                return False
+        print('qualified check: pass')
+
+        #print(f'test passed for {file.name}') TODO: print out file name being used
+        return True
+    
 
 
-def getLatest(path):
+class Input:
     """
-    return the latest file from path based on file's last change time
+    Functions take input data to create a Schedule object
     """
-    files = os.listdir(path)
-    paths = [os.path.join(path, fileName) for fileName in files]
-    return max(paths, key = os.path.getctime)
+
+    INPUT_DIR = 'Input/'
+
+    def scheduleFrom(jsonFile, matchingAlgorithm):
+        """
+        create a schedule object from json input with specified matching algorithm
+        """
+        with open(jsonFile) as file:
+            scheduleData = file.read()
+            schedule = json.loads(scheduleData)
+            roles = [parseRole(role) for role in schedule["roles"]]
+            staff = [parseStaff(staff) for staff in schedule["staff"]]
+
+            return Schedule(roles=roles, staff=staff, matchingAlgorithm=matchingAlgorithm )
+
+    def getLatest(path):
+        """
+        return the latest file from path based on file's last change time
+        """
+        files = os.listdir(path)
+        paths = [os.path.join(path, fileName) for fileName in files]
+        return max(paths, key = os.path.getctime)
 
 
-def saveSchedule(schedule, inputName):
+class Output:
     """
-    save schedule JSON to tests/output directory
-    output file name is based on name of input file
+    Functions that save schedule data to the output directory
     """
-    OUTPUT_PATH = 'tests/output'
-    outputFileName = inputName.replace('roleStaff','schedule')
-    outputPath = os.path.join(OUTPUT_PATH, outputFileName)
 
-    with open(outputPath, 'w') as file:
-        json.dump(schedule.toJSON(), file, indent=4)
+    OUTPUT_DIR = 'Output/'
+
+    def saveJSON(schedule, inputName):
+        """
+        save schedule JSON to tests/output directory
+        output file name is based on name of input file
+        """
+        OUTPUT_PATH = 'tests/output'
+        outputFileName = inputName.replace('roleStaff','schedule')
+        outputPath = os.path.join(Output.OUTPUT_DIR, outputFileName)
+
+        with open(outputPath, 'w') as file:
+            json.dump(schedule.toJSON(), file, indent=4)
+
+    def saveCSV(schedule, outputName):
+        """
+        write a schedule's schedule to a csv file with specified output name
+        #TODO: schedule input information inside Schedule?
+        e.g. schedule.Date from incomming json    
+        """
+        file = ''.join([Output.OUTPUT_DIR, outputName,'.csv'])
+
+        with open(file, 'w', newline='') as csvFile:
+            csvWriter = csv.writer(csvFile)
+            csvData = schedule.toCSV()
+
+            for row in csvData:
+                csvWriter.writerow(row)
 
 
-INPUT_PATH = "tests/input"
-inputFilePath = getLatest(INPUT_PATH)
+inputFilePath = getLatest(Input.INPUT_PATH)
 
 # create schedule
 schedule = createSchedule(inputFilePath)
