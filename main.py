18e0653683cbf8a0b3cc5da081a084c04facc3e3
Sil Demmer <Sil@Hey.com>
2023-08-10 15:13:09 -0400

    added algorithm argument

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import logging
 import repairDoubles
 from Schedule import Schedule
+from MatchingAlgorithms import MatchingAlgorithms
 
 logger = logging.getLogger(__name__)
 
 def createSchedule(roleCollection, staffCollection):
 
-    schedule = Schedule(roles=roleCollection, staff=staffCollection)
+    schedule = Schedule(roles=roleCollection, staff=staffCollection, matchingAlgorithm=MatchingAlgorithms.weightedMatching)
     schedule.logSchedule()
 
     repairDoubles.repairDoubles(schedule)
