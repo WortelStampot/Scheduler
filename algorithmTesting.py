a4d4404007e5ded08a4f5f148f64e3e89fd2bc60
Sil Demmer <Sil@Hey.com>
2023-08-17 16:57:29 -0400

    8/7 data output including role preference ratios

--- algorithmTesting.py
+++ algorithmTesting.py
@@ -3,7 +3,7 @@ from MatchingAlgorithms import MatchingAlgorithms
 from repairDoubles import repairDoubles
 
 
-jsonFile = InputOutput('roleStaff_8_7_strict.json')
+jsonFile = InputOutput('roleStaff_8_7_open.json')
 algorithm = MatchingAlgorithms.weightedMatching
 
 schedule = jsonFile.scheduleWith(algorithm)
