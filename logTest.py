db986dcd6f140e32678b4842df464cbb264ebeb4
Sil Demmer <Sil@Hey.com>
2023-08-19 15:06:11 -0400

    using InputFile class having access to the schedule object directly is nice

--- logTest.py
+++ logTest.py
@@ -1,9 +1,10 @@
-from InputOutput import InputOutput
+from InputOutput import InputFile, scheduleFrom
 from MatchingAlgorithms import MatchingAlgorithms
 import logging
 logging.basicConfig(filename='activity.log', filemode='w', level=logging.INFO, format='%(funcName)s() - %(asctime)s %(levelname)s: %(message)s', datefmt='%H:%M:%S')
 
-jsonFile = InputOutput('roleStaff_8_7_strict.json')
+jsonInput = InputFile('roleStaff_8_7_strict.json')
 algorithm = MatchingAlgorithms.weightedMatching
 
-schedule = jsonFile.scheduleWith(algorithm)
+schedule = scheduleFrom(jsonInput, algorithm)
+schedule.logSchedule()
