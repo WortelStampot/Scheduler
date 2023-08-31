e03c5a5617ea5c0b33dd6ed8ba4a432c2f1a942b
Sil Demmer <Sil@Hey.com>
2023-08-31 12:23:12 -0400

    definition for isOpenForOverlap

--- callTimeOverlap.py
+++ callTimeOverlap.py
@@ -32,6 +32,46 @@ def isCallTimeOverlap(role, schedule):
     logger.warning(f'True, {role} preceeded by {precedingRole}')
     return True
 
+
+
+def isOpenForOverlap(staff, role, schedule):
+    """
+    Return True if staff can be reassigned to role without causing overlapping shifts
+    """
+    #take into account repaired doubles
+    allDays = {day for day in Weekdays}
+    daysWorking = {role.day for role in staff.shifts(schedule)}
+    openDays = allDays - daysWorking
+
+    if role.day not in openDays:
+        return False
+    
+    
+    shifts = staff.shifts(schedule)
+    precedingRole = None
+    #if calltime is early, check if staff is working a late shift the day before
+    if role.callTime.hour < 12:
+        #find role in preceding day, check calltime
+        precedingDay = role.day.value - 1
+        for shift in shifts:
+            if shift.day.value == precedingDay:
+                precedingRole = shift
+        if precedingRole is None or precedingRole.callTime.hour < 17:
+            return True
+
+    succeedingRole = None
+    #if calltime is late, check if staff is working a morning shift the day after
+    if role.callTime.hour >= 17:
+        #find role in following day, check calltime
+        succeedingDay = role.day.value + 1
+        for shift in shifts:
+            if shift.day.value == succeedingDay:
+                succeedingRole = shift
+        if succeedingRole is None or precedingRole.callTime.hour >= 12:
+            return True
+    
+    return False
+
 overlappingRoles = [isCallTimeOverlap(shift)]
 
 schedule.repair(overlap)
