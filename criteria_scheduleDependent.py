2fa287eeca4105420267317df37b282db0453b0d
Sil Demmer <Sil@Hey.com>
2023-09-03 19:43:42 -0400

    simple 'isOpenFor_Doubles'

--- criteria_scheduleDependent.py
+++ criteria_scheduleDependent.py
@@ -61,7 +61,7 @@ def isOpenFor(self, role, schedule):
     Returns True when Staff is not working on Role.day and Staff is available for Role.callTime
     """
     allDays = {day for day in Weekdays}
-    daysWorking = {role.day for role in self.shifts(schedule)}
+    daysWorking = {role.day for role in self.shifts(schedule)} #iterate through schedule once per call
     openDays = allDays - daysWorking
 
     staffDoesNotWorkThisRole = True #take into account staff being scheduled for this role
@@ -71,6 +71,69 @@ def isOpenFor(self, role, schedule):
     return (role.day in openDays and staffDoesNotWorkThisRole) and self.isAvailable(role) and self.isQualified(role)
 
 
+def isOpenFor_Doubles(staff, role, schedule):
+    '''
+    identify if a staff is open to work the role.
+    'open' defined by doubles criteria
+        False when staff is already matched with a role on this role's day
+    '''
+    staffDaysWorking = [shift.day for shift in staff.shifts(schedule)] #get the days that this staff is working.
+        #we iterate through the schedule to find all of this staff's shifts.
+    
+    #with all the staff's shifts, we have all the days they are scheduled-
+    #now we can check when this role's day is in the staff's working days.
+    return role.day not in staffDaysWorking
+
+
+def createGraph_Doubles(schedule):
+    '''
+    this graph is a representation of all the shifts a staff is open to swap with
+    based on the doubles criteria.
+    We call isOpenFor_doubles for each possible role/staff combination of the schedule.
+    the returned True or False value is what makes up the square matrix of this graph.
+    '''
+
+    #We call isOpenFor_doubles for each possible role/staff combination of the schedule
+    for staff in schedule.staff:
+        for role in schedule.roles:
+            isOpenFor_Doubles(staff, role, schedule)
+
+    #we want to store this True/False value
+    # we also want to store the role and staff associated with this True False value
+    #as a set, these True/False values make up a square matrix of 'rows and column'
+    # we can now use this matrix as a graph to find chains
+    # a staff being open for a role, and the staff of that role being open for another role
+    # when the staff of the current end point of a chain is open for the role which started the chain,
+    # this chain is closed loop- a cycle
+    # a cycle means all staff involved can swap into each others role,
+    # and since these True/False values are derived from the doubles criteria,
+    # this cycle of swaps, repairs all staff and 'fixes' the double.
+
+    
+    #the returned True or False value is what makes up the square matrix of this graph
+
+
+
+
+    pass
+
+'''
+schedule.repair(doubles) is: 
+
+to dentify double shifts
+    doubles definition - isDouble(shift) True/False
+    doubleRoles = [role for role, staff in schedule.schedule.items() if isDouble(role)]
+
+to make the graph
+    graph is a dictionary of True/False values for each staff
+
+the graph is a reference for each shift of the schedule.
+
+for each shift of the schedule, the staff of the shift is checked to see if they are open to swap with this shift.
+
+a list of shifts, with a True/False pairing when this staff is open to swap with this shift.
 
 
 schedule.repair(doubles)
+
+'''
