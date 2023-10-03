74208e6636aa4b1e4bcf1c892029169bc36ad7ff
Sil Demmer <Sil@Hey.com>
2023-10-03 18:52:35 -0400

    test cases for doubles.openFor

--- test_Criteria.py
+++ test_Criteria.py
@@ -49,5 +49,77 @@ class Test_Doubles():
         schedule.matching = {tuesdayRole: staff_2, tuesdayRole_2: staff_2}
         assert Doubles.check(staff, tuesdayRole, schedule) == False
 
+    def test_openFor01(self):
+        """
+        assert True
+        Staff is not working that day and they are available
+        """
 
+        staff = Staff(name='atlas', maxShifts=4, availability=None)
+        role = Role(name='Aux', day=Weekdays.TUESDAY, callTime=datetime.time(hour=18, minute=00))
+        role.qualifiedStaff = ['atlas']
+        schedule = Schedule(roles=[], staff=[], matchingAlgorithm=MatchingAlgorithms.test_emptyMatching)
+        schedule.matching = {}
+
+        isAvailabile = {Weekdays.MONDAY: [],
+                Weekdays.TUESDAY: [datetime.time(18,0)],
+                Weekdays.WEDNESDAY: [],
+                Weekdays.THURSDAY: [],
+                Weekdays.FRIDAY: [],
+                Weekdays.SATURDAY: [],
+                Weekdays.SUNDAY: []}
+
+        staff.availability = isAvailabile
+        assert Doubles.isOpenFor(staff, role, schedule)
+        assert Doubles.isOpenFor_WithDoubles(staff, role, schedule)
+
+    def test_openFor02(self):
+        """
+        assert False
+        Staff is working that day and they are available
+        """
+
+        staff = Staff(name='atlas', maxShifts=4, availability=None)
+        role = Role(name='Aux', day=Weekdays.TUESDAY, callTime=datetime.time(hour=18, minute=00))
+        role.qualifiedStaff=['atlas']
+        anotherTuesdayRole = Role(name='Front', day=Weekdays.TUESDAY, callTime=datetime.time(4,30))
+        anotherTuesdayRole.qualifiedStaff=['atlas']
+        schedule = Schedule(roles=[], staff=[], matchingAlgorithm=MatchingAlgorithms.test_emptyMatching)
+        schedule.matching = {anotherTuesdayRole: staff}
+
+        isAvailabile = {Weekdays.MONDAY: [],
+                Weekdays.TUESDAY: [datetime.time(18,0)],
+                Weekdays.WEDNESDAY: [],
+                Weekdays.THURSDAY: [],
+                Weekdays.FRIDAY: [],
+                Weekdays.SATURDAY: [],
+                Weekdays.SUNDAY: []}
+        
+        staff.availability = isAvailabile
+        assert Doubles.isOpenFor(staff, role, schedule) == False
+        assert Doubles.isOpenFor_WithDoubles(staff, role, schedule) == False
+
+    def test_openFor03(self):
+        """
+        assert False
+        Staff is not working that day and they are not available
+        """
+
+        staff = Staff(name='atlas', maxShifts=4, availability=None)
+        role = Role(name='Aux', day=Weekdays.TUESDAY, callTime=datetime.time(hour=18, minute=00))
+        role.qualifiedStaff=['atlas']
+        schedule = Schedule(roles=[], staff=[], matchingAlgorithm=MatchingAlgorithms.test_emptyMatching)
+        schedule.matching = {}
+
+        notAvailabile = {Weekdays.MONDAY: [],
+                Weekdays.TUESDAY: [],
+                Weekdays.WEDNESDAY: [],
+                Weekdays.THURSDAY: [],
+                Weekdays.FRIDAY: [],
+                Weekdays.SATURDAY: [],
+                Weekdays.SUNDAY: []}
+        
+        staff.availability = notAvailabile
+        assert Doubles.isOpenFor(staff, role, schedule) == False
+        assert Doubles.isOpenFor_WithDoubles(staff, role, schedule) == False
 
