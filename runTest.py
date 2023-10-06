82d282f57869f7b9de96e897dd4602f1df71bbc1
Sil Demmer <Sil@Hey.com>
2023-10-05 22:16:26 -0400

    basic schedule test

--- runTest.py
+++ runTest.py
@@ -1,6 +1,9 @@
-from tests.logTest import test
+from tests.scheduleTest import test
+from tests.InputOutput import InputFile
 
-test() # run the test
+input = InputFile('roleStaff_8_7_open.json')
+
+test(input) # run the test
 
 #git hub actions
 # ci/cd
---
+++ tests/scheduleTest.py
@@ -0,0 +1,27 @@
+from tests.InputOutput import InputFile
+import json
+from parsingFunctions import parseRole, parseStaff
+from main import createSchedule
+from Criteria import Doubles, CallTimeOverlap
+
+def test(input):
+    with open(input.path) as file:
+        scheduleData = file.read()
+        schedule = json.loads(scheduleData)
+        roles = [parseRole(role) for role in schedule["roles"]]
+        staff = [parseStaff(staff) for staff in schedule["staff"]]
+
+    schedule = createSchedule(roles, staff)
+
+    doubles = schedule.identify(Doubles)
+    overlaps = schedule.identify(CallTimeOverlap)
+    unassigned = schedule.unassignedRoles
+
+    assert len(doubles) <= 2
+    print(f'doubles count: {len(doubles)}')
+
+    assert len(overlaps) <= 4
+    print(f'overlap count: {len(overlaps)}')
+
+    assert len(unassigned) <= 6
+    print(f'unassigned count: {len(unassigned)}')
