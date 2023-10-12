68f7585fe33713f3b8fa2c39e258d556fffbcbea
Sil Demmer <Sil@Hey.com>
2023-10-12 13:04:07 -0400

    setting swing Roles aside

--- Schedule.py
+++ Schedule.py
@@ -16,7 +16,7 @@ class Schedule:
 		self.matchingAlgorithm = matchingAlgorithm
 		self.matching = matchingAlgorithm(self.roles, self.staff)
 		self.unassignedRoles = [role for role in self.roles if role not in self.matching]
-		self.unrepaired = {} # dict to be filled during repairSchedule process
+		self.unassigned = {} # dict to be filled with {'Criteria': [Roles]} during the createSchedule process
 		self.staffDays = {staff: {matchedRole.day: matchedRole for matchedRole, matchedStaff in self.matching.items() 
 							if matchedStaff.name == staff.name } for staff in self.staff}
 
--- main.py
+++ main.py
@@ -8,8 +8,17 @@ logger = logging.getLogger(__name__)
 
 def createSchedule(roleCollection, staffCollection):
 
+    #set swing roles aside
+    swingRoles = [role for role in roleCollection if role.name =='swing']
+    #and remove them before the initial matching
+    for role in swingRoles:
+        roleCollection.remove(role)
+
     schedule = Schedule(roles=roleCollection, staff=staffCollection, matchingAlgorithm=MatchingAlgorithms.weightedMatching)
     schedule.logSchedule()
+    
+    #add swing roles to schedule.unassinged['Swing']
+    schedule.unassigned['Swing'] = swingRoles
 
     repairSchedule(schedule, Doubles)
     repairSchedule(schedule, CallTimeOverlap)
