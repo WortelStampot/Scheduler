563a7d5e636631c1890efc276a5923378b775cfc
Sil Demmer <Sil@Hey.com>
2023-10-13 12:24:10 -0400

    log test update

--- tests/logTest.py
+++ tests/logTest.py
@@ -9,7 +9,7 @@ logging.basicConfig(filename='activity.log', filemode='w', level=logging.INFO, f
 logger = logging.getLogger(__name__)
 
 
-jsonInput = InputFile('roleStaff_10_2_strict.json')
+jsonInput = InputFile('roleStaff_10_2_strict_maxShiftCount.json')
 algorithm = MatchingAlgorithms.weightedMatching
 
 schedule = scheduleFromMain(jsonInput)
