a28079f8eeff9f2906b54b36b701c70b93ce16aa
Sil Demmer <Sil@Hey.com>
2023-10-14 13:26:10 -0400

    tidying up: added removeRoles function

--- main.py
+++ main.py
@@ -8,16 +8,9 @@ logger = logging.getLogger(__name__)
 
 def createSchedule(roleCollection, staffCollection):
 
-    #set swing roles aside
-    swingRoles = [role for role in roleCollection if role.name =='swing']
-    #and remove them before the initial matching
-    for role in swingRoles:
-        roleCollection.remove(role)
-
-    #set aside 'uber' roles
-    uberRoles = [role for role in roleCollection if role.name == 'uber']
-    for role in uberRoles:
-        roleCollection.remove(role)
+    #remove roles for consistant testing
+    swingRoles = removeRoles(roleCollection, 'swing')
+    uberRoles = removeRoles(roleCollection, 'uber') 
 
     schedule = Schedule(roles=roleCollection, staff=staffCollection, matchingAlgorithm=MatchingAlgorithms.weightedMatching)
 
@@ -25,7 +18,7 @@ def createSchedule(roleCollection, staffCollection):
     schedule.unassigned['Initial Matching'] = [
         role for role in schedule.roles if role not in schedule.matching]
     
-    #add uber and swing roles to schedule.unassinged
+    #add removed roles to schedule.unassinged
     schedule.unassigned['Swing'] = swingRoles
     schedule.unassigned['Uber'] = uberRoles
 
@@ -35,3 +28,19 @@ def createSchedule(roleCollection, staffCollection):
     repairSchedule(schedule, CallTimeOverlap)
     
     return schedule
+
+
+def removeRoles(roleCollection: list, roleName: str) -> list:
+    """
+    remove 'swing' roles from the roleCollection
+    return list of removed roles
+
+    this is done to match the 'strict' input being used
+    can reintroduce these roles into the matching process-
+    after forming a stable understanding of the output
+    """
+    removedRoles = [role for role in roleCollection if role.name == roleName]
+    for role in removedRoles:
+        roleCollection.remove(role)
+
+    return removedRoles
