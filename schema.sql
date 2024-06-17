4e5392483b801e9b04978035730a1d5977a04761
Sil Demmer <Sil@Hey.com>
2024-06-17 00:35:40 -0400

    adding input / output files schema.sql

--- schema.sql
+++ schema.sql
@@ -6,9 +6,12 @@ Create Table staff (
     name TEXT NOT NULL
     );
 
+CREATE TABLE 
+
 
 CREATE TABLE role (
 id INTEGER PRIMARY KEY
+-- integer primary key autoincrements in sqlite
 name TEXT NOT NULL
 callTime TEXT NOT NULL
 -- calltime text stored in HH:MM:SS format
